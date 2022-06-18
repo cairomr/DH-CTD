cb6135af66758aa39de65c84c9fe732722b3f5bd
Cairo Oliveira <cairo.mr@gmail.com>
2022-06-17 22:39:57 -0300

    Update frases

--- progImperativa/A21/ex21.js
+++ progImperativa/A21/ex21.js
@@ -7,8 +7,17 @@ alunos_array.push(
     alunos('Breno','M',3,[7,9,9],'7B'),
     alunos('Carla','F',4,[8,7,7],'7B'),
     alunos('Monique','F',5,[5,10,7],'7B'),
-    alunos('Bianca','F',6,[3,2,7],'7B'),
-    alunos('Andreia','F',7,[10,10,9],'7B')
+    alunos('Bianca','F',6,[10,2,7],'7B'),
+    alunos('Andreia','F',7,[10,10,9],'7B'),
+    alunos('Enzo','M',8,[7,10,10],'7B'),
+    alunos('Sophia','F',9,[5,7,7],'7B'),
+    alunos('Sheyla','F',10,[10,10,10],'7B'),
+    alunos('Moana','F',11,[8,9,7],'7B'),
+    alunos('Mawe','M',12,[9,9,9],'7B'),
+    alunos('Olaf','M',13,[2,2,2],'7B'),
+    alunos('Elza','F',14,[9,9,9],'7B'),
+    alunos('Anna','F',11,[8,9,7],'7B'),
+    alunos('Petter Pan','M',12,[3,5,9],'7B')
     );
 // funcao construtora
 function alunos(nome,sexo,id,notas,turma){
@@ -26,7 +35,7 @@ function media(array){
     return (array.reduce((soma,notas)=>soma+=notas, 0) / array.length);
 }
 //Funcao para verificar o sexo
-function mediaPorSexo(array,sexoEscolhido){
+function tipoSexo(array,sexoEscolhido){
     return array.filter(
         function(array){
             return (array.sexo === sexoEscolhido);
@@ -34,17 +43,15 @@ function mediaPorSexo(array,sexoEscolhido){
     )
 }
 
-/* Funcao de tirar a media das notas
- function media2(array){
+//Funcao de tirar a media das notas
+ function contador(array){
     let soma = 0;
-    let total = 0;
     for (let n of array){
-            soma += n;
+            soma++;
     }
-    total = soma/array.length;
-    return total;
+    return soma;
  }
-*/
+
 //Funcao que separa a media dos alunos e cria uma array de retorno
 function separarMedia(array){
     return array.map(function(a){
@@ -53,12 +60,19 @@ function separarMedia(array){
 }
 // Funcao que tira a media geral da sala
 function main(array){
+    let masc = 'M';
+    let femi = 'F';
     let rstMediaSala = media(separarMedia(array)).toFixed(2);
-    let msgMediaSala = `A sala tem ${array.length} alunos.\nA média da sala é: ${rstMediaSala}`
-    let rstMediaSexoM = media(separarMedia(mediaPorSexo(alunos_array,'M'))).toFixed(2);
-    let msgMediaSexoM = `A Media do sexo Masculino é:` + rstMediaSexoM;
-    let rstMediaSexoF = media(separarMedia(mediaPorSexo(alunos_array,'F'))).toFixed(2);
-    let msgMediaSexoF = `A Media do sexo Feminino é:` + rstMediaSexoF;
+    let meninos = contador(tipoSexo(array,masc));
+    let meninas = contador(tipoSexo(array,femi));
+    let msgMediaSala = `\nA sala tem ${array.length} alunos.
+            \nMeninos da sala: ${meninos}
+            \nMeninas da sala: ${meninas}
+            \nA média da sala é: ${rstMediaSala}\n`
+    let rstMediaSexoM = media(separarMedia(tipoSexo(alunos_array,masc))).toFixed(2);
+    let msgMediaSexoM = `A média do sexo Masculino é: ${rstMediaSexoM}`;
+    let rstMediaSexoF = media(separarMedia(tipoSexo(alunos_array,femi))).toFixed(2);
+    let msgMediaSexoF = `A média do sexo Feminino é: ${rstMediaSexoF}`;
     return `${msgMediaSala}\n${msgMediaSexoM}\n${msgMediaSexoF}`;
 }
 // Comando para visualizar o array de alunos
